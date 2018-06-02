48e1b8a44e78cf6bdaf79a78a9ada29100e71da0
Alex Bush <alexabush7@gmail.com>
2018-06-02 16:03:39 -0700

    working on check win algorithm

--- connect-four/src/App.js
+++ connect-four/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isWin: false,
       isPlayer1Turn: true,
       board: Array.from({ length: 3 }, val => {
         return Array.from({ length: 3 }, val => 0);
@@ -26,6 +27,32 @@ class App extends Component {
         }
         newState.isPlayer1Turn = !newState.isPlayer1Turn;
       }
+      return newState;
+    });
+    this.checkWin();
+  };
+
+  checkWin = () => {
+    this.setState(prevState => {
+      const newState = { ...prevState };
+      const currentBoard = newState.board;
+      let p1Wins,
+        p2Wins = false;
+      currentBoard.map((row, rowIndex) => {
+        //find way to map columns
+        //find way to map each diagonal
+        if (row.every(square => square === 1)) p1Wins = true;
+        if (row.every(square => square === 2)) p2Wins = true;
+      });
+
+      //need way to get all 1s and all 2s from a column
+      currentBoard.reduce((row, rowIndex) => {
+        debugger;
+        row.map((value, column) => {
+          debugger;
+        });
+      });
+
       return newState;
     });
   };
